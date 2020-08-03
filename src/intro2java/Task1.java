984b3029400e35033149a6297d099dba064b1ce5
fdw <florina@danciu-wendt.net>
2020-08-03 23:03:27 +0200

    task1 commit

---
+++ src/intro2java/Task1.java
@@ -0,0 +1,20 @@
+package intro2java;
+
+
+
+public class Task1 {
+
+    public static class SumOfHundred {
+        public void calc () {
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("dosomething");
+
+        //new SumOfHundred().calc();
+    }
+}
+
+
