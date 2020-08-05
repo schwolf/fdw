56dc80b2b81ba244fea7f6ad8a1dc8a753f33848
fdw <florina@danciu-wendt.net>
2020-08-05 21:31:30 +0200

    Task1 added

--- src/intro2java/Task1.java
+++ src/intro2java/Task1.java
@@ -4,16 +4,27 @@ package intro2java;
 
 public class Task1 {
 
+    // Calculate the sum of the first 100 numbers higher than 0
+
     public static class SumOfHundred {
         public void calc () {
+
+            int nr = 100;
+            int sumNr =0;
+
+            for (nr=1; nr <=100; nr++) {
+                sumNr=sumNr+nr;
+                System.out.println(sumNr);
+            }
         }
+
 
     }
 
     public static void main(String[] args) {
-        System.out.println("dosomething");
 
-        //new SumOfHundred().calc();
+
+        new SumOfHundred().calc();
     }
 }
 
